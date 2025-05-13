2346c1147899f5e559471390df945803ad37dbb6
Vahvaselka <vilkkuvahvaselka@gmail.com>
2025-05-13 20:22:04 +0300

    Initial commit

---
+++ src/main/java/com/example/calendarEl/CalendarElApplication.java
@@ -0,0 +1,13 @@
+package com.example.calendarEl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarElApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarElApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/calendarEl/CalendarElApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calendarEl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarElApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
